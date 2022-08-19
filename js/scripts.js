ebc8566a308d39866f594b3e44fb32209ba7ba73
1Sayd <person.tak@gmail.com>
2022-08-19 11:37:34 +0300

    add js script. Hover main-nav

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-alert('Hello JavaScript');
+const nav_item = document.querySelectorAll('.main-nav__items');
+const nav_home = document.querySelector('.main-nav__items');
+
+function isOverNavElement() {
+  setTimeout(() => {
+    nav_item.forEach(node => node.classList.remove('main-nav__items--current'));
+    this.classList.add('main-nav__items--current');
+  }, 200);
+}
+
+function isOutNavElement() {
+  setTimeout(() => {
+    this.classList.remove('main-nav__items--current');
+    nav_home.classList.add('main-nav__items--current');
+  }, 200);
+}
+
+nav_item.forEach(node => {
+    node.addEventListener('mouseover', isOverNavElement);
+    node.addEventListener('mouseout', isOutNavElement);
+  }
+);
